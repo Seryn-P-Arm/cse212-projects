908e5fd87a7f3c5ba6df52b7eee246cc7b65ce65
Seryn Armstrong <serynarmstrong@gmail.com>
2025-10-09 14:09:44 +0200

    completed W06 team activity

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,9 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        // If the value already exists, do nothing (no duplicates allowed)
+        if (value == Data)
+            return;
 
         if (value < Data)
         {
@@ -34,12 +37,21 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data && Left != null)
+            return Left.Contains(value);
+        else if (value > Data && Right != null)
+            return Right.Contains(value);
+        else
+            return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
